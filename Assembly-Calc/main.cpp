02b85263c58fd68d7632b108204099e93818e43a
Sman664 <sman664@hotmail.com>
2021-06-04 16:43:13 -0500

    AND, OR, SHR, SHL instructions added but not tested

--- Assembly-Calc/main.cpp
+++ Assembly-Calc/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	std::cout << "Hello! Please enter in this format: <instruction> <operand1> <operand2>" << std::endl;
 	std::cout << "NOTE: All operands should be entered in Hex" << std::endl;
-	std::cout << "The format also operates similarly to assembly code" << std::endl;
+	std::cout << "The format also operates exactly like assembly code" << std::endl;
 	
 	uintptr_t answer;
 	
@@ -53,6 +53,23 @@ int main()
 
 			answer = num;
 		}
+		//shift instructions not fully tested but are assumed to work correctly on 64 bit integers
+		else if (instr == "shl")
+		{
+			uintptr_t num;
+			num = operand1<<operand2;
+			std::cout << std::hex << num << std::endl;
+
+			answer = num;
+		}
+		else if (instr == "shr")
+		{
+			uintptr_t num;
+			num = operand1>>operand2;
+			std::cout << std::hex << num << std::endl;
+
+			answer = num;
+		}
 		else if (instr == "add")
 		{
 			uintptr_t num;
@@ -69,11 +86,28 @@ int main()
 
 			answer = num;
 		}
+		//AND and OR instructions not fully tested but are assumed to work correctly on 64 bit integers
+		else if (instr == "or")
+		{
+			uintptr_t num;
+			num = operand1 | operand2;
+			std::cout << std::hex << num << std::endl;
+
+			answer = num;
+		}
+		else if (instr == "and")
+		{
+			uintptr_t num;
+			num = operand1 & operand2;
+			std::cout << std::hex << num << std::endl;
+
+			answer = num;
+		}
 		//This has been tested with 64 bit hex values
 		else if (instr == "xor")
 		{
 			uintptr_t num;
-			num = operand1^operand2;
+			num = operand1 ^ operand2;
 			std::cout << std::hex << num << std::endl;
 
 			answer = num;
